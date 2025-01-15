5da9deec7848de04cfa56d5ef2fa0e0a897c90ae
CosmicDan <dconnolly.au@gmail.com>
2025-01-15 17:36:11 +1100

    Update inBedChatFixes comment

--- sleepingoverhaul-common/src/main/java/github/cosmicdan/sleepingoverhaul/client/ClientConfig.java
+++ sleepingoverhaul-common/src/main/java/github/cosmicdan/sleepingoverhaul/client/ClientConfig.java
@@ -16,11 +16,11 @@ public class ClientConfig {
     private static final String bedRestOnEnterTxt = " If enabled, pressing enter on In Bed Chat Screen with zero chat text will also function as Sleep button.\n" +
             " Requires bedRestEnabled in server config, otherwise does nothing otherwise.";
     public final ForgeConfigSpec.BooleanValue inBedChatFixes;
-    private static final String inBedChatFixesTxt = " Enhances/fixes keyboard-navigation issues on the In-Bed chat screen. Summary:\n" +
-            "     - Allows to re-focus the chat box by clicking on it;\n" +
-            "     - Prevents arrow keys from changing focus away from chat box, use Ctrl+Tab to change focus instead;\n" +
-            "     - Prevents ENTER from sending a chat message unless the chat box is actually focused;\n" +
-            "     - Allows ENTER to actually work if one of the buttons is focused instead of the chat box.\n" +
+    private static final String inBedChatFixesTxt = " [1.19.4+ only] Enhances/fixes keyboard-navigation issues on the In-Bed chat screen. Summary:\n" +
+            " - Allows to re-focus the chat box by clicking on it;\n" +
+            " - Prevents arrow keys from changing focus away from chat box, use Ctrl+Tab to change focus instead;\n" +
+            " - Prevents ENTER from sending a chat message unless the chat box is actually focused;\n" +
+            " - Allows ENTER to actually work if one of the buttons is focused instead of the chat box.\n" +
             " You will probably want to keep this enabled because of the buttons we add to the screen, but it can be disabled in case there are mod conflicts.";
 
     public ClientConfig(final ForgeConfigSpec.Builder builder) {
